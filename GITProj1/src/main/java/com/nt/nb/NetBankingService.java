a3f8c375390bce00eac85b1d549b8c82d1d2709e
PraveenSaiReddy <chagantipraveensaireddy@gmail.com>
2026-06-02 15:40:01 +0530

    Dev1 improved the b2 Branch

--- GITProj1/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj1/src/main/java/com/nt/nb/NetBankingService.java
@@ -2,9 +2,9 @@ package com.nt.nb;
 
 public class NetBankingService 
 {
-	public String TransferMoney(long srcAcno,long destacno,double amt) 
+	public String TransferMoney(long srcAcno,long destacno,double amt,String gateway) 
 	{
-		return amt+"amount is transfereed from "+srcAcno+"to"+destacno;
+		return amt+"amount is transfereed from "+srcAcno+"to"+destacno+"using gateway"+gateway;
 		
 	}
 
